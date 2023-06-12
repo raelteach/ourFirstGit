9d3a1fd192221b11fd5ddd33f078bc4dbef2cc9e
Rael Liss <rael.lissoos@iacd.io>
2023-06-12 15:18:58 +0200

    we added a sum function

--- gittest.playground/Contents.swift
+++ gittest.playground/Contents.swift
@@ -2,3 +2,13 @@ import UIKit
 
 var greeting = "Hello, GIT"
 print(greeting)
+
+
+// should we add sum function
+
+func sum(_ num1: Int, _ num2: Int) -> Int{
+    
+    return num1 + num2
+}
+
+print(sum(10, 15))
