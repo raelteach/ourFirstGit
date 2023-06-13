f80fd85d56128862a2951053a0243ea3dddefbcd
Rael Liss <rael.lissoos@iacd.io>
2023-06-13 14:10:06 +0200

    we added a multiplication function

--- gittest.playground/Contents.swift
+++ gittest.playground/Contents.swift
@@ -14,3 +14,11 @@ func sum(_ num1: Int, _ num2: Int) -> Int{
 print(sum(10, 15))
 
 print("we have staged and committed")
+
+
+func mult(_ num1: Int, _ num2: Int) -> Int{
+    
+    return num1 * num2
+}
+
+print(mult(10, 25))
